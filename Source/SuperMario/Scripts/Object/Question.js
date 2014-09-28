84e7f3fd830e0b27e2aa7a971d3ecd9fff4415b7
Leon <yang73137@126.com>
2014-09-28 22:24:59 +0800

    Change counter.

--- Source/SuperMario/Scripts/Object/Question.js
+++ Source/SuperMario/Scripts/Object/Question.js
@@ -11,7 +11,7 @@
         this.sprite.setBackgroundImage("../Images/TileSet.png");
         this.sprite.setBackgroundPosition(32 * 24, 0);
         this.sprite.setFrameSequence([{ x: 32 * 24, y: 0 }, { x: 32 * 25, y: 0 }, { x: 32 * 26, y: 0 }]);
-        this.sprite.frameCounter.setCount(12);
+        this.sprite.frameCounter.setCount(15);
         this.sprite.setRepeat(0);
         this.sprite.show();
 
