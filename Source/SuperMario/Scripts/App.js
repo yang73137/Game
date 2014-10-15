5f26e9d18bdbd47e309f4fdb99fa6f3ed4a28463
Leon <yang73137@126.com>
2014-10-15 10:13:01 +0800

    Add app.

--- Source/SuperMario/Scripts/App.js
+++ Source/SuperMario/Scripts/App.js
@@ -1,104 +1,38 @@
-﻿UIMode = {
-    Opening: 0,
-    Stage: 1,
-    Game: 2,
-    Score: 3,
-    Ending: 4
-};
-
+﻿
 App = {
     init: function () {
-        this.container = document.getElementById("app");
+        var app = document.getElementById("app");
 
-        this.openingUI = new OpeningUI();
-        this.openingUI.appendTo(this.container);
+        var gameUI = new GameUI();
+        gameUI.setSize(Const.SCREEN_WIDTH, Const.SCREEN_HEIGHT);
+        gameUI.show();
+        gameUI.appendTo(app);
 
-        this.stageUI = new StageUI();
-        this.stageUI.appendTo(this.container);
+        gameUI.setMario(new MarioBors(0, 0));
 
-        this.gameUI = new GameUI();
-        this.gameUI.appendTo(this.container);
+        var world = new World_1_1();
+        world.addToGameUI(gameUI);
 
-        this.scoreUI = new ScoreUI();
-        this.scoreUI.appendTo(this.container);
-
-        this.endingUI = new EndingUI();
-        this.endingUI.appendTo(this.container);
-
-        this.mode = UIMode.Opening;
+        this.gameUI = gameUI;
     },
     process: function (src, loaded, total) {
-        /*
-        console.log(src + " has been loaded");
-        */
+        console.log("Loading: " + src + "   " + loaded + "/" + total);
+        document.getElementById("app").innerHTML = "Loading: " + src + "   " + loaded + "/" + total;
     },
     complete: function () {
-
+        document.getElementById("app").innerHTML = "";
         App.init();
-
-        this.openingUI.enter();
         App.onTimer();
     },
     error: function (src, loaded, total) {
         alert("加载图片 " + src + " 出错");
-        /*
-        console.log(src + " is error");
-        */
     },
     run: function () {
-        ImageLoader.load(this, [Const.IMAGE_BOOM.src, Const.IMAGE_MISC.src, Const.IMAGE_TANK.src, Const.IMAGE_TERR.src, Const.IMAGE_UI.src]);
+        ImageLoader.load(this, ["../Images/Enemies.png", "../Images/Items.png", "../Images/MarioBros4.png", "../Images/TileSet.png", "../Images/World_1_1.png", "../Images/World_1_2.png"]);
     },
     onTimer: function () {
         setInterval(function () {
-            var app = App;
-            switch (app.mode) {
-                case UIMode.Opening:
-                    if (!app.openingUI.update()) {
-                        app.openingUI.level();
-                        app.stageUI.enter();
-                        app.mode = UIMode.Stage;
-                    }
-                    break;
-                case UIMode.Stage:
-                    if (!app.stageUI.update()) {
-                        app.stageUI.level();
-                        app.gameUI.setStage(app.stageUI.stage);
-                        app.gameUI.enter();
-                        app.mode = UIMode.Game;
-                    }
-                    break;
-                case UIMode.Game:
-                    if (!app.gameUI.update()) {
-                        app.gameUI.level();
-                        var scoreData = app.gameUI.getScoreData();
-                        app.scoreUI.setData(scoreData.stage, scoreData.player1Score, scoreData.enemy1Number, scoreData.enemy2Number, scoreData.enemy3Number, scoreData.enemy4Number);
-                        app.scoreUI.enter();
-                        app.mode = UIMode.Score;
-                    }
-                    break;
-                case UIMode.Score:
-                    if (!app.scoreUI.update()) {
-                        app.scoreUI.level();
-                        if (app.gameUI.isFailed()) {
-                            app.stageUI.setStage(1);
-                            app.stageUI.setStageChangedEnabled(true);
-                            app.endingUI.enter();
-                            app.mode = UIMode.Ending;
-                        } else {
-                            app.stageUI.setStage(++app.stageUI.stage);
-                            app.stageUI.enter();
-                            app.mode = UIMode.Stage;
-                        }
-                    }
-                    break;
-                case UIMode.Ending:
-                    if (!app.endingUI.update()) {
-                        app.endingUI.level();
-                        app.openingUI.enter();
-                        app.mode = UIMode.Opening;
-                    }
-                    break;
-            }
+            App.gameUI.update();
         }, 16);
     }
 };
